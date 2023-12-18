1ac57947732f2a294339188c77b3164492aabeb8
reddysai616 <62210706+reddysai616@users.noreply.github.com>
2023-12-18 22:10:01 +0530

    config

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,9 +6,6 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.19",
   networks: {
-    matic: {
-      url: `https://polygon-mumbai.infura.io/v3/9c0ea514ddc040059a5426506c2f12ed`,
-      accounts:["c57828f5b54c5f3fa21619871ff454567adf25703e74da0038a3b3c02c3615d5"]
-    }
+   
   }
 };
